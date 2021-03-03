c9db050cc81636a10801c41f359c6ca57c19d69a
Skryfall <skryfall@gmail.com>
2021-03-03 13:29:09 -0600

    [ADD] Log in Python

--- app-server/src/handler.py
+++ app-server/src/handler.py
@@ -1,55 +1,211 @@
 import sys
 import json
 import histogram
+from datetime import datetime
+import os
 
 dirC = None
 dirH = None
 dirL = None
-jsonData = None
+images = None
+histNum = None
+rHistNum = None
+classNum = None
+rClassNum = None
+client = None
+genError = None
+op = None
 
-def readJson():
-    global jsonData
+def createStringAux2(message, status):
+    message += "Status: " + status + ", "
+    message += "Datetime: " + str(datetime.now()) + "\n"
 
-    with open("images.json") as f:
-        jsonData = json.load(f)
-        f.close()
-    print(jsonData["client"])
-    if not (jsonData["images"]):
-        print("Error, there are no images to handle")
+    return message
+
+def createStringAux(message):
+        if genError is not None:
+            return createStringAux2(message, genError)
+        else:
+            return createStringAux2(message, "Completed Successfuly")
+
+def createString():
+    global rHistNum
+    global rClassNum
+
+    message = ""
+    files = "Null, "
+    filesAux = ""
+
+    if client is not None:
+        message += "Client: " + client + ", "
+    if str(op) == "0":
+        if images is not None:
+            for x in images:
+                filesAux += "histogram" + str(rHistNum) + ".png, "
+                rHistNum += 1
+            files = filesAux
+
+        message += "Files: " + files
+        return createStringAux(message)
+    else:
+        if images is not None:
+            for x in images:
+                filesAux += "class" + str(rClassNum) + ".png, "
+                rClassNum += 1
+            files = filesAux
+
+        message += "Files: " + files
+        return createStringAux(message)
+
+def writeLog():
+    message = createString()
+
+    try:
+        with open("." + dirL + "/log.txt", "a") as f:
+            f.write(message)
+            f.close()
+        return
+    except:
+        with open("." + dirL + "/log.txt", "w") as f:
+            f.write(message)
+            f.close()
+        return
+
+def readImages():
+    global images
+    global client
+    global genError
+
+    try:
+        with open("images.json") as f:
+            jsonData = json.load(f)
+            f.close()
+
+        if not (jsonData["images"]):
+            genError = "Error, there are no images to handle"
+            print(genError)
+            writeLog()
+            sys.exit()
+
+        client = jsonData["client"]
+        images = jsonData["images"]
+        return
+    except:
+        genError = "Error, couldn't open images.json"
+        print(genError)
+        writeLog()
         sys.exit()
-
-    return
 
 def setFilePaths(readData):
     global dirC
     global dirH
     global dirL
+    global genError
 
-    dirC = readData[0][12:]
-    dirH = readData[1][10:]
-    dirL = readData[2][8:]
-    
-    return
+    try:
+        dirC = readData[0][12:]
+        dirH = readData[1][10:]
+        dirL = readData[2][8:]
+        return
+    except:
+        genError = "Error, there was a problem while reading the directories"
+        print(genError)
+        sys.exit()
 
 def readConfigFile():
+    global genError
+
     readData = []
     linesToRead = [1, 2, 3]
 
-    with open("config.conf") as f:
-        for position, line in enumerate(f):
-            if position in linesToRead:
-                readData.append(str.rstrip(line))
-        f.close()
+    try:
+        with open("config.conf") as f:
+            for position, line in enumerate(f):
+                if position in linesToRead:
+                    readData.append(str.rstrip(line))
+            f.close()
 
-    return setFilePaths(readData)
+        return setFilePaths(readData)
+    except:
+        genError = "Error, couldn't open config.conf"
+        print(genError)
+        sys.exit()
+
+def readData():
+    global histNum
+    global classNum
+    global rHistNum
+    global rClassNum
+
+    try:
+        with open("data.json") as f:
+            jsonData = json.load(f)
+            f.close()
+        histNum = jsonData["histNum"]
+        classNum = jsonData["classNum"]
+        rHistNum = histNum
+        rClassNum = classNum
+        return
+    except:
+        histNum = 0
+        classNum = 0
+        data = {}
+        data["histNum"] = histNum
+        data["classNum"] = classNum
+        with open("data.json", "w") as f:
+            json.dump(data, f)
+            f.close()
+        return
+
+def updateDataAux():
+    data = {}
+    data["histNum"] = histNum
+    data["classNum"] = classNum
+    with open("data.json", "w") as f:
+        json.dump(data, f)
+        f.close()
+    return
+
+def updateData():
+    global histNum
+    global classNum
+
+    quantImages = len(images)
+
+    if str(op) == "0":
+        histNum += quantImages
+        return updateDataAux()
+    else:
+        classNum += quantImages
+        return updateDataAux()
+
+def checkStatus(status):
+    if status:
+        updateData()
+        writeLog()
+        return
+    else:
+        writeLog()
+        sys.exit()
+
+def cleanFiles():
+    os.remove("./images.json")
+    return
 
 def main():
-    readJson()
-    readConfigFile()
+    global genError
+    global op
 
-    if str(sys.argv[1]) == "0":
-        histogram.applyHistogram(jsonData["images"], dirH)
-        return
+    readConfigFile()
+    readImages()
+    readData()
+    #cleanFiles()
+
+    op = sys.argv[1]
+
+    if str(op) == "0":
+        status, genError = histogram.applyHistogram(images, histNum, dirH)
+        return checkStatus(status)
     else:
         print("ES 1")
         return
--- app-server/src/histogram.py
+++ app-server/src/histogram.py
@@ -2,7 +2,11 @@ from PIL import Image, ImageOps
 import base64
 import io
 
-def applyHistogramAux(image, dir):
+histError = None
+
+def applyHistogramAux(image, number, direc):
+    global histError
+
     try:
         if image.mode == 'RGBA':
             r, g, b, a = image.split()
@@ -10,21 +14,36 @@ def applyHistogramAux(image, dir):
             equalizedImage = ImageOps.equalize(rgbImage, mask = None)
             r2,g2,b2 = equalizedImage.split()
             finalEqualizedImage = Image.merge('RGBA', (r2,g2,b2,a))
-            finalEqualizedImage.save("." + dir + "/" + "test1" + ".png")
+            finalEqualizedImage.save("." + direc + "/histogram" + str(number) + ".png")
             return 1
         else:
             equalizedImage = ImageOps.equalize(image, mask = None)
-            equalizedImage.save("." + dir + "/" + "test2" + ".png")
+            equalizedImage.save("." + direc + "/histogram" + str(number) + ".png")
             return 1
     except:
-        print("An error ocurred while applying the histogram equalization, exiting")
+        histError = "An error ocurred while applying the histogram equalization, exiting"
+        print(histError)
         return 0
 
-def applyHistogram(imageList, dir):
-    for x in imageList:
+def applyHistogram(b64ImageList, number, direc):
+    imageList = {}
+    imagesSizes = {}
+    index = 0
+ 
+    for x in b64ImageList:
         image = base64.b64decode(str(x))       
         image = Image.open(io.BytesIO(image))
-        if not applyHistogramAux(image, dir):
-            return 0
+        w, h = image.size
+        imageSize = w * h
+        imagesSizes[index] = imageSize
+        imageList[index] = image
+        index += 1
     
-    return 1
+    imagesSizes = sorted(imagesSizes.items(), key=lambda x: x[1])
+
+    for i in imagesSizes:
+        if not applyHistogramAux(imageList[i[0]], number, direc):
+            return 0, histError     
+        number += 1
+
+    return 1, histError
