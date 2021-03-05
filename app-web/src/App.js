837079aaf16b5b48a0dedc2f9f29bbeaab613b65
Juan <juanpo.alvarado@gmail.com>
2021-03-05 17:18:02 -0600

    [UPDATE] Webpage v2

--- app-web/src/App.js
+++ app-web/src/App.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useState } from "react";
 import ImageUploader from "react-images-upload";
 
@@ -6,6 +7,33 @@ import bg from "./assets/bg-img.jpg";
 
 function App() {
   const [server, setServer] = useState("");
+  const [port, setPort] = useState("");
+  const [client, setClient] = useState("");
+  const [images, setImages] = useState([]);
+
+  const convertBase64 = (file) => {
+    return new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(file);
+      fileReader.onload = () => {
+        resolve(fileReader.result);
+      };
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+  };
+
+  const send = async (event) => {
+    event.preventDefault();
+    const imageArray = [];
+    for (let img of images) {
+      const image64 = await convertBase64(img);
+      imageArray.push(image64.substring(23));
+    }
+    console.log({ client, images: imageArray });
+    //axios.post(`${server}:${port}/hist`, { client, images });
+  };
 
   return (
     <div className="App">
@@ -14,24 +42,43 @@ function App() {
         <h1 className="Title">Image web</h1>
         <form>
           <input
+            className="Input"
             value={server}
             onChange={(e) => setServer(e.target.value)}
             type="text"
             placeholder="Server Ip"
           ></input>
+          <input
+            className="Input"
+            value={port}
+            onChange={(e) => setPort(e.target.value)}
+            type="text"
+            placeholder="Puerto"
+          ></input>
+          <input
+            className="Input"
+            value={client}
+            onChange={(e) => setClient(e.target.value)}
+            type="text"
+            placeholder="Cliente"
+          ></input>
           <ImageUploader
             withIcon
             withPreview
             label=""
-            singleImage
             buttonText="Subir imagen"
-            onChange={(e) => console.log(e)}
-            imgExtension={[".jpg", ".gif", ".png", ".gif"]}
+            onChange={(imgs) => setImages(imgs)}
+            imgExtension={[".jpg", ".jpeg", ".png", ".gif"]}
             maxFileSize={5242880}
           />
-          <button className="Button" onClick={(e) => e.preventDefault()}>
-            Confirmar
-          </button>
+          <div className="BtnContainer">
+            <button className="Button" onClick={send}>
+              Histograma
+            </button>
+            <button className="Button" onClick={send}>
+              Colores
+            </button>
+          </div>
         </form>
       </div>
     </div>
